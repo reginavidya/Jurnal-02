60a5bc8b5535f74851ccde79af7e55ff22f169ab
reginavidya <reginavidya@student.telkomuniversity.co.id>
2020-09-17 14:16:34 +0700

    HelloWorld

---
+++ praktikum/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Regina");
+		System.out.println("NIM: D3IF-6706202126");
+		System.out.println("Kelas: 44-03");
+	}
+}
